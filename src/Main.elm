037da65baf5a7bdc1bcd7702af70cee79f19aaa6
tomforster <e1x1c2a3l5gh>
2019-12-21 15:29:36 +0000

    draw some circles in a loop with delay and max count

--- src/Main.elm
+++ src/Main.elm
@@ -1,61 +1,81 @@
 module Main exposing (main)
 
-import Browser
-import Html exposing (Html, Attribute, div, input, text)
-import List exposing (map)
+import Browser exposing (Document)
 import Svg exposing (Svg, svg, circle)
+import Html exposing (Html)
 import Svg.Attributes exposing (..)
 import Random
+import Time
 
-main = Browser.element {
-    init = init,
-    update = update,
-    view = view,
-    subscriptions = subscriptions }
+main = Browser.document
+    {
+        init = init,
+        update = update,
+        view = view,
+        subscriptions = subscriptions
+    }
 
-type alias Model
-    = {circle: Maybe CircleDef}
+type alias Model =
+    {
+        circles: List CircleDef
+    }
+
+type alias CircleDef =
+    {
+        x : Float,
+        y : Float,
+        radius: Float,
+        color: Int
+    }
+
+type alias Document msg =
+    {
+        title : String,
+        body : List (Html msg)
+    }
+
+type Msg = CircleMsg CircleDef | Tick Time.Posix
 
 -- MODEL
 
 init : () -> (Model, Cmd Msg)
 init _ = (
-        {circle = Maybe.Nothing},
-        Random.generate CircleMsg (Random.map4 genCircleDef (Random.float 15 100) (Random.float 15 90) (Random.float 5 15) (Random.int 1 255))
+        {circles = []},
+        makeCircleMsg
     )
-
 
 -- UPDATE
 
-type Msg = CircleMsg CircleDef
-
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of CircleMsg circleDef -> ({model | circle = Maybe.Just circleDef}, Cmd.none)
+    case msg of
+        CircleMsg circleDef ->
+            if List.length model.circles < 100
+                then ({model | circles = model.circles ++ [circleDef]}, Cmd.none)
+            else ({model | circles = Maybe.withDefault [] (List.tail model.circles) ++ [circleDef]}, Cmd.none)
 
+        Tick _ -> (model, makeCircleMsg)
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
-subscriptions _ = Sub.none
+subscriptions _ =
+  Time.every 50 Tick
+
+-- VIEW
+
+view : Model -> Document Msg
+view model =
+    {body = [
+        svg [style "width:100%; height:100%"] (List.map buildCircle model.circles)
+    ], title = "Demo"}
 
 genCircleDef: Float -> Float -> Float -> Int -> CircleDef
 genCircleDef x y radius color = {x = x, y = y, radius = radius, color = color}
 
-type alias CircleDef =
-    {
-    x : Float,
-    y : Float,
-    radius: Float,
-    color: Int
-    }
+makeCircleMsg : Cmd Msg
+makeCircleMsg = Random.generate CircleMsg (Random.map4 genCircleDef (Random.float 0 100) (Random.float 0 100) (Random.float 15 160) (Random.int 0 255))
 
 buildCircle : CircleDef -> Svg element
 buildCircle circleDef =
-    circle [cx (String.fromFloat circleDef.x), cy (String.fromFloat circleDef.y), r (String.fromFloat circleDef.radius), fill ("#" ++ String.fromInt(circleDef.color))] []
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-    svg [style "margin: 1em"] [(buildCircle (Maybe.withDefault {x = 1, y =1, radius = 1, color = 5} model.circle))]
+    circle [cx (String.fromFloat circleDef.x ++ "%"), cy (String.fromFloat circleDef.y ++ "%"), r (String.fromFloat circleDef.radius), fill ("hsl(" ++ String.fromInt(circleDef.color) ++ ", 100%, 50%)")] []
